2ca4ebcb5973ab52d6aaecd5826a4157b83cf951
AlejandraFruto <a.fruto@uniandes.edu.co>
2024-02-10 16:24:02 -0500

    cambios de model

--- App/model.py
+++ App/model.py
@@ -2,3 +2,27 @@
 El modelo se encarga de la creacion de funciones y manejo de 
 logica del programa
 """
+
+variables=[]
+
+def verificar(linea:str)->bool:
+    if "defvar" in linea:
+        parametros = linea.split()
+        defvar(parametros[1], parametros[3])
+        return True
+    #otras funciones   
+    else:
+        return False
+
+
+   
+
+def defvar(name:str, n:int )->bool:
+    variables.append(name)
+    
+    
+    syntax = True
+    return  syntax
+
+    
+    
