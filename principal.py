a4a7b9f57555c2670f0a47a32c4883f7d5ed18c1
dvargasl2 <d.vargasl2@uniandes.edu.co>
2024-02-10 15:03:29 -0500

    docstring

---
+++ principal.py
@@ -0,0 +1,11 @@
+    """
+    
+    Proyecto 0 LYM, 202410_ISIS-1106_S03
+    Alejandra Fruto - 00000000, Daniel Vargas 202123892
+    a.fruto@uniandes.edo.co, d.vargasl2@uniandes.edu.co
+    
+    El programa debe leer un archivo txt con una sitaxis,
+    y programa debe verificar si la sintaxis de este es correcta 
+    y devolver "yes" o "no" como respuesta de la verificacion de la sintaxis.
+    
+    """
