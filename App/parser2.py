399f721f3f4cfd1d1aaf698e4af8b90f4d6aeae1
AlejandraFruto <a.fruto@uniandes.edu.co>
2024-02-18 22:54:24 -0500

    cambios parser 2

--- App/parser2.py
+++ App/parser2.py
@@ -1,5 +1,8 @@
 from collections import namedtuple
 import config as cf
+import sys
+sys.setrecursionlimit(100000000)
+
 # Define tokens
 Token = namedtuple("Token", ["type", "value"])
 KEYWORD = "KEYWORD"
@@ -52,7 +55,10 @@ def keywords_verification(current_token: str) -> str:
             return CONDITION
     for keyword in KEYWORDS.keys():
         if keyword in current_token:
-            return KEYWORD
+            return KEYWORD  
+    for keyword in COMANDS.keys():
+        if keyword in current_token:
+            return COMAND
     return STRING
 
 # Define function to tokenize input
@@ -78,6 +84,10 @@ def tokenize(text):
 
 
 # Define functions to check syntax and consistency
+
+defined_variables = {}
+defined_functions = {}
+        
 def is_valid_variable(name):
     if name in defined_variables:
         return True
@@ -90,10 +100,10 @@ def is_valid_function_call(name, params):
     expected_params = defined_functions[name]["params"]
     return len(expected_params) == len(params) and all(is_valid_variable(p) for p in params)
 
-
 def is_valid_condition(condition, tokens):
     lista_dir = [":north", ":south", ":west", ":east"]
     lista_c_p = ["chips", "balloons"]
+    
     if "facing?" in condition:
         parts = condition.split("facing?")
         for element in parts:
@@ -118,7 +128,7 @@ def is_valid_condition(condition, tokens):
                         if len(info) < 2:
                             info.append(name)
                             info.append(n)
-        if info[0] in lista_c_p and n.isdigit():
+        if info[0] in lista_c_p and info[1].isdigit():
             return True
         else:
             return False
@@ -134,7 +144,7 @@ def is_valid_condition(condition, tokens):
                         if len(info) < 2:
                             info.append(name)
                             info.append(n)
-        if info[0] in lista_c_p and n.isdigit():
+        if info[0] in lista_c_p and info[1].isdigit():
             return True
         else:
             return False
@@ -162,10 +172,6 @@ def is_valid_condition(condition, tokens):
     if "not" in condition: 
         return True
                     
-            
-        
-
-
 def parentesis_organizados(cadena):
     subcadenas = []
     nivel_parentesis = 0
@@ -181,40 +187,7 @@ def parentesis_organizados(cadena):
                 subcadenas.append(cadena[inicio_subcadena:i+1])
                 inicio_subcadena = i + 1
     return subcadenas
-
-def verificar_condicion_if(tokens):
-    verificados = []
-    for token in tokens:
-        if token.type == CONDITION:
-            if is_valid_condition(token.value, tokens) == True:
-                verificados.append(True)
-            else:
-                verificados.append(False)
-    if False in verificados:
-        return False
-    else:
-        return True
                 
-
-def parentesis_organizados(cadena):
-    subcadenas = []
-    nivel_parentesis = 0
-    inicio_subcadena = 0
-    for i, char in enumerate(cadena):
-        if char == '(':
-            nivel_parentesis += 1
-            if nivel_parentesis == 1:
-                inicio_subcadena = i
-        elif char == ')':
-            nivel_parentesis -= 1
-            if nivel_parentesis == 0:
-                subcadenas.append(cadena[inicio_subcadena:i+1])
-                inicio_subcadena = i + 1
-    return subcadenas
-
-defined_variables = {}
-defined_functions = {}
-    
 def save_functions_variables(tokens):
     lista_constantes = ['Dim', 'myXpos', 'myYpos', 'myChips', 'myBaloons', 'ballonsHere', 'ChipsHere', 'Spaces']
     params = []
@@ -248,16 +221,243 @@ def save_functions_variables(tokens):
             if 'defun' in token.value:
                 nombre = token.value[5:]
                 defined_functions[nombre] = params
-    print(defined_functions) 
+   
+def is_valid_comand(command):
+    lista_dir = [":north", ":south", ":east", ":west"]
+    lista_move = [":left", ":right", ":around"]
+    lista_constantes = ['Dim', 'myXpos', 'myYpos', 'myChips', 'myBaloons', 'ballonsHere', 'ChipsHere', 'Spaces']
+    lista_c_p = ["chips", "balloons"]
+    lista_move_dir = [ ":front", ":right", ":left", ":back"]
+    
+    if "defvar" in command:
+        info = []
+        cadena_info = command[6:]
+        for caracter in cadena_info:
+                            if caracter.isdigit():
+                                posicion = cadena_info.find(caracter)
+                                n = cadena_info[posicion:]
+                                name = cadena_info[:posicion]
+                                info = [name, n]
+                                break
+        for constante in lista_constantes:
+                            if constante in cadena_info:
+                                posicion = cadena_info.find(constante)
+                                n = cadena_info[posicion:]
+                                if n!=None:
+                                    name = cadena_info[:posicion]
+                                info = [name, n] 
+                                
+        if info[1].isdigit() or info[1] in lista_constantes:   
+            return True
+        else:
+            return False 
+    
+    if "=" in command:
+        info = []
+        cadena_info = command[1:]
+        for caracter in cadena_info:
+                            if caracter.isdigit():
+                                posicion = cadena_info.find(caracter)
+                                n = cadena_info[posicion:]
+                                name = cadena_info[:posicion]
+                                info = [name, n]
+                                break
+        for constante in lista_constantes:
+                            if constante in cadena_info:
+                                posicion = cadena_info.find(constante)
+                                n = cadena_info[posicion:]
+                                if n!=None:
+                                    name = cadena_info[:posicion]
+                                info = [name, n] 
+                                
+        if info[0]  in defined_variables:
+            defined_variables[info[0]] = info[1]
+            return True
+        else:
+            return False       
+        
+    if "move" in command:
+        parts = command.split("move")
+        for element in parts:
+            if element == "":
+                parts.remove(element)
+        if parts[0] in lista_constantes or parts[0] in defined_variables or parts[0].isdigit():
+            return True
+        else:
+            return False
+        
+    if "skip" in command:
+        parts = command.split("skip")
+        for element in parts:
+            if element == "":
+                parts.remove(element)
+        if parts[0] in lista_constantes or parts[0] in defined_variables or parts[0].isdigit():
+            return True
+        else:
+            return False
+        
+    if "turn" in command:
+        parts = command.split("turn")
+        for element in parts:
+            if element == "":
+                parts.remove(element)
+        if parts[0] in lista_move:
+            return True
+        else:
+            return False
+        
+    if "face" in command:
+        parts = command.split("face")
+        for element in parts:
+            if element == "":
+                parts.remove(element)
+        if parts[0] in lista_dir:
+            return True
+        else:
+            return False
+        
+    if "put" in command:
+        info = []
+        cadena_info = command[3:]
+        for caracter in cadena_info:
+                            if caracter.isdigit():
+                                posicion = cadena_info.find(caracter)
+                                n = cadena_info[posicion:]
+                                name = cadena_info[:posicion]
+                                info = [name, n]
+                                break
+        for constante in defined_variables:
+                            if constante in cadena_info:
+                                posicion = cadena_info.find(constante)
+                                n = cadena_info[posicion:]
+                                if n!=None:
+                                    name = cadena_info[:posicion]
+                                info = [name, n] 
+                             
+        if info[0] in lista_c_p and (info[1].isdigit() or info[1] in defined_variables):
+           return True
+        else:
+            return False  
+        
+    if "pick" in command:
+        info = []
+        cadena_info = command[4:]
+        for caracter in cadena_info:
+                            if caracter.isdigit():
+                                posicion = cadena_info.find(caracter)
+                                n = cadena_info[posicion:]
+                                name = cadena_info[:posicion]
+                                info = [name, n]
+                                break
+        for constante in defined_variables:
+                            if constante in cadena_info:
+                                posicion = cadena_info.find(constante)
+                                n = cadena_info[posicion:]
+                                if n!=None:
+                                    name = cadena_info[:posicion]
+                                info = [name, n] 
+                             
+        if info[0] in lista_c_p and (info[1].isdigit() or info[1] in defined_variables):
+           return True
+        else:
+            return False
+        
+    if "move-dir" in command:  
+        info = []
+        cadena_info = command[8:]
+        for constante in lista_move_dir:
+                                if constante in cadena_info:
+                                    posicion = cadena_info.find(constante)
+                                    n = cadena_info[posicion:]
+                                    if n!=None:
+                                        name = cadena_info[:posicion]
+                                    info = [name, n] 
+        if ( info[0] in defined_variables or info[0].isdigit() ) and info[1] in lista_move_dir:
+                return True
+        else:
+               return False
+    
+    if "run-dirs" in command:
+        parts = command.split("run-dirs")
+        for element in parts:
+            if element == "":
+                parts.remove(element)
+        cleaned_string = parts[0].replace("[", "").replace("]", "").replace(" ", "")
+        
+        partss = cleaned_string.split(",")
+        visitados = []
+        for element in partss:
+            if element in lista_move_dir:
+                visitados.append(True)
+            else: 
+                visitados.append(False)
+             
+        if len(partss) != 0:
+            if False in visitados:
+                return False
+            else:
+                return True
+        else:
+            return False
+        
+    if "move-face" in command:
+        info = []
+        cadena_info = command[9:]
+        for constante in lista_dir:
+                                if constante in cadena_info:
+                                    posicion = cadena_info.find(constante)
+                                    n = cadena_info[posicion:]
+                                    if n!=None:
+                                        name = cadena_info[:posicion]
+                                    info = [name, n] 
+        if ( info[0] in defined_variables or info[0].isdigit() ) and info[1] in lista_dir:
+                return True
+        else:
+               return False
+        
+    if "null" in command:
+        return True
+    
+def verificar_if_loop(tokens):
+    verificados_condition = []
+    verificados_command = []
+    for token in tokens:
+        if token.type == CONDITION:
+            if is_valid_condition(token.value, tokens) == True:
+                verificados_condition.append(True)
+            else:
+                verificados_condition.append(False)
+        if token.type == COMAND:
+            if is_valid_comand(token.value) == True:
+                 verificados_command.append(True)
+            else:
+                verificados_command.append(True)
+                
+    if False in verificados_condition or False in verificados_command:
+        return False
+    else:
+        return True
+    
+def verificar_repeat_times():
+    pass
+    
             
 def check_syntax(tokens):
-    current_block = []
-    for token in tokens:
+    
+    """for token in tokens:
      
         if token.type == SEPARATOR:
             continue
-        if token.type == KEYWORD or COMAND:
-            if "defvar" or 'defun' in token.value:
+        if token.type == COMAND:
+            if "defvar"  in token.value:
+                save_functions_variables(tokens)
+                if is_valid_comand(token.value):
+                    return True
+                else:
+                    return False
+            
+        if token.type == KEYWORD :
+            if 'defun' in token.value:
                 save_functions_variables(tokens)
                 continue
             elif token.value == "defun":
@@ -277,7 +477,7 @@ def check_syntax(tokens):
                 defined_functions[next_token.value] = {"params": params}
                 continue
             elif token.value == "if":
-                verificar_condicion_if(tokens)
+                verificar_if_loop(tokens)
                 
             elif token.value in ["loop", "repeat"]:
                 if not check_syntax(current_block):
@@ -291,14 +491,11 @@ def check_syntax(tokens):
         elif token.type == VARIABLE:
             if token.value not in defined_variables and token.value not in defined_functions:
                 return False
-        elif token.type == STRING:
-            if token.value not in ["north", "south", "east", "west", "chips", "balloons"]:
-                return False
         current_block.append(token)
   
     if len(current_block) > 0 and not check_syntax(current_block):
         return False
-    return True
+    return True"""
 
 def iniciar(lista):
     return main(lista)
@@ -306,7 +503,7 @@ def iniciar(lista):
 def main(lista):
     bandera = True
     #for linea in lista:
-    linea = '(defunfoo(c,p)(put:chipsc)(put:balloonsp)(moverotate))'
+    linea = '(defunfill()(repeatSpaces(if(not(isZero?myChips))(put:chips1)(null))))'
     tokenlinea = tokenize(linea)
     print(tokenlinea)
     if check_syntax(tokenlinea)is False:
